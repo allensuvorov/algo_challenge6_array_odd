4e0c71f3d7e72f77565b4608dd485d33cc5f6b74
Allen <allenvs@mail.ru>
2020-02-17 20:54:08 +0300

    wrapped the code into a function

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-var arr=[];
-for (let i=1;i<50;i+=2) arr.push(i);
-console.log(arr);
+array_odd = () => {
+    var arr=[];
+    for (let i=1;i<50;i+=2) arr.push(i);
+    console.log(arr);
+};
+array_odd();    
